f2c913641d2f03cad3eb820bc35a7db80dc2ed42
Sebastian <sebastian.sarviala@gmail.com>
2023-11-10 10:13:51 +0200

    Refaktoroitu @reduxjs/toolkit-toimintoja note reducerin osalta

--- src/index.js
+++ src/index.js
@@ -1,25 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 import filter from './reducers/filterReducer'
 import notes from './reducers/noteReducer'
-import { createNote } from './reducers/noteReducer'
-import { filterChange } from './reducers/filterReducer'
 
-const reducers = combineReducers({ filter, notes })
-const store = createStore(reducers)
-
-console.log(store.getState())
+const store = configureStore({
+  reducer: {
+    filter,
+    notes
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 )
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
